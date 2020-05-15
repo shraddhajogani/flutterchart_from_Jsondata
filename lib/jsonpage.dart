d1d87fa4d55d2c230786da6d3380c84201c3358d
Shraddha <jogani.shraddha@gmail.com>
2020-05-15 10:03:08 +0200

    + Added dart convert + Decode the data

--- lib/jsonpage.dart
+++ lib/jsonpage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
-
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   final Widget child;
@@ -14,14 +14,21 @@ class _HomePageState extends State<HomePage> {
   List<charts.Series<Sales, String>> seriesBarData;
 
   _generateData() async {
-    final decoded = await DefaultAssetBundle.of(context).loadString("asset/data.json") as List;
+    final load =
+    await DefaultAssetBundle.of(context).loadString("asset/data.json");
+    var decoded = json.decode(load);
+    List<Sales> sales = [];
+    for (var item in decoded) {
+      sales.add(Sales.fromJson(item));
+    }
 
     seriesBarData.add(charts.Series(
-      data:decoded,
+      data: sales,
       domainFn: (Sales sales, _) => sales.saleyear,
       measureFn: (Sales sales, _) => int.parse(sales.saleval),
       id: 'Performance',
     ));
+    setState(() {});
   }
 
   @override
@@ -33,32 +40,32 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.cyan,
         title: Text('flutter charts'),
       ),
-      body:  Column(
+      body: Column(
         children: [
           Text(
             'Sales By Year',
-            style: TextStyle(
-                fontSize: 24.0, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
           ),
           SizedBox(
             height: 10.0,
           ),
-          Expanded(
+          seriesBarData.length > 0
+              ? Expanded(
             child: charts.BarChart(
               seriesBarData,
               animate: true,
               animationDuration: Duration(seconds: 5),
             ),
-          ),
+          )
+              : Container(),
         ],
       ),
     );
-
   }
 }
 
@@ -66,7 +73,10 @@ class Sales {
   String saleyear;
   String saleval;
 
-
-  // Add Constructor
   Sales(this.saleyear, this.saleval);
+
+  Sales.fromJson(Map<String, dynamic> json) {
+    saleyear = json['saleyear'];
+    saleval = json['saleval'];
+  }
 }
